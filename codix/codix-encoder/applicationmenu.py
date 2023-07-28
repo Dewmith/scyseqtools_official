645da46922edd7266f081381ddcede7ed462bef7
lrtpzd <lpezard@free.fr>
2023-07-28 15:18:40 +0200

    1/ Create a truecodix.py file instead of encoder which only keeps the Apllication stuff to start the application.
    2/ some pylint adaptations

--- codix/codix-encoder/applicationmenu.py
+++ codix/codix-encoder/applicationmenu.py
@@ -1,50 +1,43 @@
+"""
+Menu for codix encoder
+"""
 import tkinter
 
 class ApplicationMenu(tkinter.Menu):
+    """
+    The menu with very few actions...
+    """
 
     def __init__(self, master=None):
 
         tkinter.Menu.__init__(self, master)
-       
         # Actions Menu
-        self.actionMenu = tkinter.Menu(self, tearoff=0)
-        self.add_cascade(label='Actions', menu=self.actionMenu)
+        self.action_menu = tkinter.Menu(self, tearoff=0)
+        self.add_cascade(label='Actions', menu=self.action_menu)
 
-        self.new_code = self.actionMenu.add_command(label='Define a new code', 
-                                                  command=master.new_code)
+        self.action_menu.add_command(label='Define a new code',
+                                     command=master.new_code)
+        self.action_menu.add_command(label='Start a new session',
+                                     command=master.start_session)
+        self.action_menu.add_command(label='Retrieve a session',
+                                     command=master.retrieve_session)
+        self.action_menu.add_separator()
+        self.action_menu.add_command(label='Reset',
+                                     command=master.reset,
+                                     state=tkinter.NORMAL)
+        self.action_menu.add_separator()
+        self.action_menu.add_command(label='Quit', command=master.quit)
 
-        self.start_session = self.actionMenu.add_command(label='Start a new session', 
-                                                       command=master.start_session)
-
-        self.retrieve_session = self.actionMenu.add_command(label='Retrieve a session', 
-                                                          command=master.retrieve_session)
-
-#        self.retrieve_session = self.actionMenu.add_command(label='Free viewing', 
-#                                                          command=master.free_view)
-
-        self.actionMenu.add_separator()
-
-        self.actionMenu.add_command(label='Reset', command=master.reset,
-                                  state=tkinter.NORMAL)
-
-#        self.actionMenu.add_separator()
-#        self.actionMenu.add_command(label='Save', command=master.save,
-#                                  state=tkinter.DISABLED)
-#        self.actionMenu.add_command(label='Save as', command=master.save_as,
-#                                  state=tkinter.DISABLED)
-
-        self.actionMenu.add_separator()
-
-        self.actionMenu.add_command(label='Quit', command=master.quit)
-        
         # Help Menu
         # FIXME: not implemented...
-        helpMenu = tkinter.Menu(self, tearoff=0)
-        self.add_cascade(label='Help', menu=helpMenu)
-        helpMenu.add_command(label='Help', command=master.help_browser)
-        helpMenu.add_command(label='About', command=master.aboutHandler)
+        help_menu = tkinter.Menu(self, tearoff=0)
+        self.add_cascade(label='Help', menu=help_menu)
+        help_menu.add_command(label='Help', command=master.help_browser)
+        help_menu.add_command(label='About', command=master.aboutHandler)
 
     def disable_actions(self):
-        # for idx in [0,1,2,3,5]:
+        """
+        When an action starts they are all disabled
+        """
         for idx in [0,1,2,4]:
-            self.actionMenu.entryconfigure(idx, state=tkinter.DISABLED)
+            self.action_menu.entryconfigure(idx, state=tkinter.DISABLED)
---
+++ codix/codix-encoder/truecodix.py
@@ -0,0 +1,376 @@
+"""
+Main application module
+
+.. todo:: All the "print" should be moved to some logging process...
+We need doc
+"""
+
+import os
+# import json
+#import time
+import pathlib
+
+import tkinter
+import tkinter.filedialog
+import tkinter.messagebox
+import tkinter.simpledialog
+
+# tk colors : https://www.tcl.tk/man/tcl/TkCmd/colors.html
+
+# from datetime import datetime # datetime.now().strftime('%c')
+
+import utils as U
+from infoframe import InfoFrame
+from playercontrol import PlayerControl
+from applicationmenu import ApplicationMenu
+from codingframe import FrameworkFrame
+from newcode import NewCode
+
+__version__ = '0.9'
+__author__ = 'L. Pezard'
+__licence__ = 'GPL'
+
+DEFAULT_CWD = '~'
+# DEFAULT_CWD = "~/Documents/videos_synchrony/vidéosynchrony/"
+
+class Application(tkinter.Tk):
+    """
+    Main codix application class
+    """
+    def __init__(self):
+        tkinter.Tk.__init__(self)
+        self.resizable(tkinter.TRUE, tkinter.TRUE)
+        # Menu
+        self.menu = ApplicationMenu(master=self)
+        self['menu'] = self.menu
+        # Variables
+        self.state = {'media_loaded': False,
+                       'code_loaded': False,
+                       'data_loaded': False}
+
+        self.__time_step = None
+
+        self.container = {'history': [], # [(date, observer, comment), ...]
+                          'media': '', # path for the media file
+                          'code': {}, # dictionary taken from the code file
+                          'times': [], # sample times (in ms) taken from the player
+                          'comments': [], # sample comments taken from codingframe
+                          'data': {},
+                          'version': __version__} # data
+
+        self._context = "standard"
+
+        initdir = os.path.expanduser(DEFAULT_CWD)
+
+        self.cwd = ''
+        while self.cwd == '':
+            self.cwd = tkinter.filedialog.askdirectory(title="Choose working directory",
+                                              initialdir=initdir,
+                                              mustexist=True)
+# FIXME: check that subfolders 'media' and 'data' exist. If not create them
+        if not os.path.exists(os.path.join(self.cwd, 'media')):
+            pathlib.Path(os.path.join(self.cwd, 'media')).mkdir()
+        if not os.path.exists(os.path.join(self.cwd, 'data')):
+            pathlib.Path(os.path.join(self.cwd, 'data')).mkdir()
+
+#        if tmp_cwd !=  '':
+#            self.cwd = tmp_cwd
+
+        print('cwd: ', self.cwd)
+        self.interface = True
+        self.recorded_steps = []
+
+
+# Interface fonction
+    def change_interface(self):
+        if self.interface :
+            self.change_color(['light_to_dark',self.control.elements,
+                                self.framework.elements, self.info.elements])
+            self.interface = False
+            self.framework.interface_button.config(text='Light mode')
+        else :
+            self.change_color(['dark_to_light', self.control.elements,
+                                self.framework.elements, self.info.elements])
+            self.framework.interface_button.config(text='Dark mode')
+            self.interface = True
+
+    def change_color(self, list):
+        if list[0] == 'light_to_dark':
+            for zone in list[1:] :
+                for elem in zone :
+                    if elem == self.control.elements :
+                        elem.config(background = self.control.dark_bg)
+                    if elem == self.info.elements :
+                        elem.config(background = self.info.dark_bg)
+                    else :
+                        elem.config(background = self.framework.dark_bg)
+        else :
+            for zone in list[1:] :
+                for elem in zone :
+                    if elem == self.control.elements :
+                        elem.config(background = self.control.light_bg)
+                    if elem == self.info.elements :
+                        elem.config(background = self.info.light_bg)
+                    else:
+                        elem.config(background = self.framework.light_bg)
+
+# Menu related functions
+#-----------------------
+
+    def new_code(self):
+        """Application for defining a new coding framework.
+        """
+        self.newcode = NewCode(parent=self)
+        self.newcode.grid(row=1, column=1, sticky=U.sticky_all)
+        self.menu.disable_actions()
+
+    def start_session(self):
+
+        self.info = InfoFrame(parent=self,
+                              states=(tkinter.NORMAL,    # load media
+                                      tkinter.DISABLED,  # load code
+                                      tkinter.DISABLED)) # load data
+        self.info.grid(row=1, column=1, sticky=U.sticky_all)
+        self.menu.disable_actions()
+
+    def retrieve_session(self):
+        self.info = InfoFrame(parent=self,
+                              states=(tkinter.DISABLED, # load code
+                                      tkinter.DISABLED, # load media
+                                      tkinter.NORMAL))  # load data
+        self.info.grid(row=1, column=1, sticky=U.sticky_all)
+        self.menu.disable_actions()
+
+    def reset(self):
+        """Reset the file menu and all the current variables.
+        """
+        raise NotImplementedError
+#        self.info.destroy()
+#        self.framework.destroy()
+#        self.control.destroy()
+#        self.init_variables()
+
+    def help_browser(self):
+        self.notimplemented()
+
+    def aboutHandler(self):
+        self.notimplemented()
+
+# Functions related to infoframe
+#-------------------------------
+
+    def make_media_player(self, fname):
+        """
+        create a player control frame
+        """
+        self.control = PlayerControl(parent=self, file_name = fname)
+        self.control.grid(row=1, column=0, sticky=U.sticky_all)
+        self.state['media_loaded'] = True
+
+    def make_coding_frame(self, fname):
+        """
+        creates the coding frame
+        """
+        self.framework = FrameworkFrame(parent=self, filename=fname)
+        self.framework.grid(row=2, column=0, sticky=U.sticky_all)
+        self.state['code_loaded'] = True
+
+    @property
+    def context(self):
+        return self._context
+
+    @context.setter
+    def context(self, value):
+        if value == "initial":
+            self._context = "processing"
+            print('Context Initial')
+            self.control.config_buttons({self.control.play_but : 'normal',
+                                    self.control.back_but : 'disabled',
+                                    self.control.forward_but : 'disabled',
+                                    self.control.mode_check : 'disabled',
+                                    self.control.period_ent : 'disabled',
+                                    self.control.time_ent: 'disabled'})
+            assert len(self.control.times) == 0
+            self.control.times.append(self.control.time)
+
+            self.container['times'] = self.control.times
+            self.container['data'] = self.framework.data
+# FIXME: check this...
+            self.container['media'] = \
+            self.info.directory_msg.cget("text") + '/' + self.info.media_file.get()
+
+
+            self.__time_step = 0
+            print('TIME_STEP: ', self.time_step)
+
+        elif value == "resume":
+            self._context = "processing"
+            print('Context Resume session')
+            #### leomodif : j'ai commenté le premier bouton et
+            #### je l'ai réactivé quand on clique sur record
+            self.control.config_buttons({#self.control.play_but : 'normal',
+                                    self.control.back_but : 'disabled',
+                                    self.control.forward_but : 'disabled',
+                                    self.control.mode_check : 'disabled',
+                                    self.control.period_ent : 'disabled',
+                                    self.control.time_ent: 'disabled'})
+
+            # assert(len(self.control.times) == 0)
+            # self.control.times.append(self.control.time)
+            # self.container['times'] = self.control.times
+            # self.container['data'] = self.framework.data
+            # self.container['media'] = self.info.media_file.get()
+
+            self.control.times = self.container['times']
+            self.framework.data = self.container['data']
+            self.framework.coding_comments = self.container['comments']
+            # self.container['media'] = self.info.media_file.get()
+
+            self.time_step = 'last'
+            print('TIME_STEP: ', self.time_step)
+
+        elif value == "processing":
+            print('Context Processing')
+            print("Recorded steps : ", self.recorded_steps)
+            self.control.play_but.config(state='normal')
+            # if self.time_step == self.times_length - 1 :
+            #     self.control.forward_but.config(state='disabled')
+            #     self.control.back_but.config(state='normal')
+            # elif self.time_step == 0 :
+            #     self.control.back_but.config(state='disabled')
+            # else:
+            #     self.control.back_but.config(state='normal')
+
+            # if len(self.recorded_steps) > self.time_step :
+            #     self.control.forward_but.config(state='normal')
+
+            if self.time_step == 0 :
+                self.control.back_but.config(state='disabled')
+            else:
+                self.control.back_but.config(state='normal')
+
+            if len(self.recorded_steps) > self.time_step :
+                self.control.forward_but.config(state='normal')
+            else :
+                self.control.forward_but.config(state='disabled')
+
+
+
+
+            self.framework.display_codes(self.time_step)
+
+        elif value == 'not_recorded':
+            self.control.config_buttons({self.control.play_but : 'disabled',
+                                    self.control.back_but : 'disabled',
+                                    self.control.forward_but : 'disabled',
+                                    self.control.mode_check : 'disabled',
+                                    self.control.period_ent : 'disabled',
+                                    self.control.time_ent: 'disabled'})
+            self.framework.config_processing_buttons('normal')
+
+
+            # self.info.save_data()
+        elif value == 'standard':
+            if self.state['code_loaded'] :
+                if self.control.state == 'paused' :
+                    self.framework.spec_frame.start_but.config(state='normal')
+
+                elif self.control.state in ['s_playing', 'c_playing']:
+                    self.framework.spec_frame.start_but.config(state='disabled')
+                    # update is needed because of the timer in regular playing
+                    self.framework.spec_frame.start_but.update()
+
+
+        else: # FIXME: raise error?
+            pass
+
+    @property
+    def times_length(self):
+        return len(self.control.times)
+
+    @property
+    def time_step(self):
+        return self.__time_step
+
+    @time_step.setter
+    def time_step(self, value):
+
+        if value == '1p':
+            if self.control.time > self.control.times[-1]:
+                self.control.times.append(self.control.time)
+                self.__time_step += 1
+            else:
+                self.__time_step = min(self.__time_step + 1, self.times_length-1)
+                self.control.time = self.control.times[self.time_step]
+            self.framework.config_processing_buttons('normal')
+
+        elif value == 1:
+            self.__time_step = min(self.__time_step + 1, self.times_length-1)
+            self.control.time = self.control.times[self.time_step]
+            self.framework.config_processing_buttons('disabled')
+
+        elif value == -1:
+            self.__time_step = max(0, self.__time_step - 1)
+            self.control.time = self.control.times[self.time_step]
+            self.framework.config_processing_buttons('disabled')
+
+        elif value == 'last':
+            self.__time_step = self.times_length - 1
+            self.control.time = self.control.times[-1]
+        else:
+            # raise error ?
+            pass
+
+        self.container['times'] = self.control.times
+        self.context = "processing"
+
+        print('TIME_STEP: ', self.time_step)
+        print(self.control.times)
+
+# FIXME: Not sure this is still needed
+#    def disable_load_data(self):
+#        self.menu.fileMenu.entryconfig(4, state=tkinter.DISABLED)
+#        self.info.data_load.config(state=tkinter.DISABLED)
+#
+#    def disable_load_media(self):
+#        self.menu.fileMenu.entryconfig(3, state=tkinter.DISABLED)
+#        self.info.media_load.config(state=tkinter.DISABLED)
+#
+#    def disable_load_code(self):
+#        self.menu.fileMenu.entryconfig(2, state=tkinter.DISABLED)
+#        self.info.code_load.config(state=tkinter.DISABLED)
+#
+#    def enable_load_data(self):
+#        self.menu.fileMenu.entryconfig(4, state=tkinter.NORMAL)
+#        self.info.data_load.config(state=tkinter.NORMAL)
+#
+#    def freeze(self):
+#        """ cannot change step
+#        """
+#        self.control.mode_check.configure(state=tkinter.DISABLED)
+#        self.control.period_ent.configure(state=tkinter.DISABLED)
+#        self.control.time_ent.configure(state=tkinter.DISABLED)
+
+#    def get_mode_and_period(self, encoding):
+#        period = encoding['period']
+#
+#        if period is None:
+#            # self.player_mode.set("continuous")
+## FIXME: est que player ne serait pa mieux que control?
+#            self.control.mode = "continuous"
+#            tkinter.messagebox.showinfo('Continuous not implemented',
+#                                   'Irregular sampling not implemented')
+#            raise NotImplementedError()
+#        else:
+#            #self.player_mode.set("regular")
+#            self.control.mode = "regular"
+#            # self.period_display.set(str(period))
+
+    def notimplemented(self):
+        tkinter.messagebox.showinfo('Not implemented', 'Not implemented')
+
+if __name__ == "__main__":
+    app = Application()
+    app.title('Codix - The Swiss knife for coding behaviors - version: %s'\
+              % __version__)
+    app.mainloop()
